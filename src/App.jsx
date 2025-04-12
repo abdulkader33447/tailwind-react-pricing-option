bc34bdf4b7e83eee3bd80e5282d140e75e776511
abdulkader33447 <abdulkader33447@gmail.com>
2025-04-12 20:21:45 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import DaisyNav from "./assets/component/DaisyNav";
+import NavBar from "./assets/component/NavBar/NavBar";
+
+function App() {
+  return (
+    <>
+      <header>
+        <DaisyNav></DaisyNav>
+        <NavBar></NavBar>
+      </header>
+
+      <main></main>
+    </>
+  );
+}
+
+export default App;
